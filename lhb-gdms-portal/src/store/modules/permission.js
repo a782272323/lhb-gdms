5f06cfd75c276be94eaf2187bdc26094ac2fcac8
a782272323 <782272323@qq.com>
2020-11-21 01:13:07 +0800

    博客门户网站前端模版搭建

---
+++ lhb-gdms-portal/src/store/modules/permission.js
@@ -0,0 +1,74 @@
+import { asyncRoutes, constantRoutes } from '@/router'
+
+/**
+ * Use meta.role to determine if the current user has permission
+ * 使用 meta.role 确定当前用户是否具有权限
+ * @param roles
+ * @param route
+ */
+function hasPermission(roles, route) {
+  if (route.meta && route.meta.roles) {
+    return roles.some(role => route.meta.roles.includes(role))
+  } else {
+    return true
+  }
+}
+
+/**
+ * Filter asynchronous routing tables by recursion
+ * 通过递归过滤异步路由表
+ * @param routes asyncRoutes
+ * @param roles
+ */
+export function filterAsyncRoutes(routes, roles) {
+  const res = []
+
+  routes.forEach(route => {
+    const tmp = { ...route }
+    if (hasPermission(roles, tmp)) {
+      // 如果当前路由有roles 字段则需要判断用户访问权限
+      if (tmp.children) {
+        // 若用户拥有的角色中有被包含在待定路由角色表中的则拥有访问权限
+        tmp.children = filterAsyncRoutes(tmp.children, roles)
+      }
+      // 没有设置roles 则无需判定即可访问
+      res.push(tmp)
+    }
+  })
+
+  return res
+}
+
+const state = {
+  routes: [],
+  addRoutes: []
+}
+
+const mutations = {
+  SET_ROUTES: (state, routes) => {
+    state.addRoutes = routes
+    state.routes = constantRoutes.concat(routes)
+  }
+}
+
+const actions = {
+  generateRoutes({ commit }, roles) {
+    return new Promise(resolve => {
+      let accessedRoutes
+      if (roles.includes('admin')) {
+        accessedRoutes = asyncRoutes || []
+      } else {
+        accessedRoutes = filterAsyncRoutes(asyncRoutes, roles)
+      }
+      commit('SET_ROUTES', accessedRoutes)
+      resolve(accessedRoutes)
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
