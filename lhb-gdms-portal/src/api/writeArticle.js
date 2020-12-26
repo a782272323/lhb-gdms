819776fb8c8fc505fb542a59291aa17c5e719814
a782272323 <782272323@qq.com>
2020-12-26 13:11:14 +0800

    门户网站:写文章和草稿箱模块前后端初步完成

---
+++ lhb-gdms-portal/src/api/writeArticle.js
@@ -0,0 +1,91 @@
+import request from '@/utils/request'
+
+// 用户写文章内容时上传的图片
+export function articleContentImg(data) {
+  return request({
+    url: '/consumer-blog/blog/write/article/content/img',
+    method: 'post',
+    data,
+    headers: { 'Content-Type': 'multipart/form-data' }
+  })
+}
+
+// 添加文章封面图片
+export function articleCoverImg(data) {
+  return request({
+    url: '/consumer-blog/blog/write/article/cover/img',
+    method: 'post',
+    data,
+    headers: { 'Content-Type': 'multipart/form-data' }
+  })
+}
+
+// 删除文章封面图片
+export function deleteArticleCoverImg(params) {
+  return request({
+    url: '/consumer-blog/blog/write/article/cover/img',
+    method: 'delete',
+    params: { key: params }
+  })
+}
+
+// 保存到草稿箱
+export function addDraft(data) {
+  return request({
+    url: '/consumer-blog/blog/write/draft',
+    method: 'post',
+    data
+  })
+}
+
+// 获取标签信息列表
+export function getLabelInfo() {
+  return request({
+    url: '/consumer-blog/blog/write/article/label',
+    method: 'get'
+  })
+}
+
+// 搜索标签信息列表
+export function queryLabelInfo(keyWord) {
+  return request({
+    url: '/consumer-blog/blog/write/article/label/query',
+    method: 'get',
+    params: { keyWord: keyWord }
+  })
+}
+
+// 发布文章
+export function faBuArticle(data, labelId) {
+  return request({
+    url: '/consumer-blog/blog/write/article',
+    method: 'post',
+    params: { labelId: labelId },
+    data
+  })
+}
+
+// 获取草稿箱列表
+export function getDraftLists() {
+  return request({
+    url: '/consumer-blog/blog/write/draft/lists',
+    method: 'get'
+  })
+}
+
+// 删除草稿箱
+export function deleteDraft(sysDraftId) {
+  return request({
+    url: '/consumer-blog/blog/write/draft/' + sysDraftId,
+    method: 'delete'
+  })
+}
+
+// 编辑草稿箱
+export function updateDraft(sysDraftId, data) {
+  return request({
+    url: '/consumer-blog/blog/write/draft/' + sysDraftId,
+    method: 'put',
+    data
+  })
+}
